4c4272455e4410accdea6f03c4f9ed668666b0af
itagakijun <itagaki.j@nexceed.co.jp>
2019-08-18 13:14:13 +0900

    member search function

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,8 +1,18 @@
 module HomesHelper
 
-  def set_core_member
+  def set_news
+    @news = News.all
   end
 
-  def set_support_member
+  def set_members
+    @core_members = Member.core_members
+  end
+
+  def set_support_members
+    @support_members = Member.support_members
+  end
+
+  def set_project_members
+    @project_members = Member.project_members(project_id)
   end
 end
