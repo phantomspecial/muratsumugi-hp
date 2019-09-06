88ef66c2b5e6701dee2c9d2952a703602d498756
itagakijun <itagaki.j@nexceed.co.jp>
2019-09-06 17:51:45 +0900

    modal member profile view

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+});
+
+// 以下jQuery
 $(document).ready(function(){
-    $('.modal-trigger').leanModal();
+    debugger;
+    $('.modal').modal();
 });
